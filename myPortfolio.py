1031c73ce04e3a943a8c0f281c8d5a9129ba73fd
Morgan Keys <morgan.keys@gmail.com>
2014-08-22 17:44:43 -0700

    Imgs and updated control file. Complete prototype.

--- myPortfolio.py
+++ myPortfolio.py
@@ -2,49 +2,33 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
+# import templating
+from jinja2 import Template
+from jinja2 import Environment, PackageLoader
 
-# configuration
-DATABASE = '/tmp/myPortfolio.db'
+# DB configuration
+DATABASE = '/tmp/portfolio.db'
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'default'
 
 
-
 # create our little application :)
 app = Flask(__name__)
 
 #app.config.from_object(__name__)
-app.config.from_envvar('myPortfolio_settings', silent=True)
+app.config.from_envvar('portfolio_settings', silent=True)
 
 #connect to database
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+#Create templating central object
+env = Environment(loader=PackageLoader('myPortfolio', 'templates'))
+
 #Home page
 @app.route('/')
-def home():
-	return render_template('home.html')
-
-#Name page
-@app.route('/@<name>')
-def hello_world(name=None):
-    return render_template('template_page.html', name=name.title())
-
-@app.route('/resume/')
-@app.route('/resume')
-def resume_page():
-	return 'It&#39;s r&#233;sum&#233;, dummy.' 
-
-@app.route('/@<name2>')
-def user_page(name2=None):
-	return render_template('user_page.html', name=name2.title())
-
-@app.route('/list_users')
-def list_users():
-	return render_template('list_users.html')
-
 @app.route('/about')
 def portfolio():
 	return render_template('portfolio.html')
@@ -52,7 +36,6 @@ def portfolio():
 @app.route('/style')
 def styleguide():
 	return render_template('styleguide.html')
-
 
 if __name__ == '__main__':
     app.run()
